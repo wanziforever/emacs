8aa97b986639f98e488a28a5cb48d5c0c8b6d2dd
wanziforever <wanziforever@gmail.com>
2025-07-21 10:17:55 +0800

    commit

---
+++ .emacs.d/elpa/consult-20250121.1423/consult-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "consult" "20250121.1423"
+  "Consulting completing-read."
+  '((emacs  "28.1")
+    (compat "30"))
+  :url "https://github.com/minad/consult"
+  :commit "31b5c191f9869aa26bedf35991635e03539a1270"
+  :revdesc "31b5c191f986"
+  :keywords '("matching" "files" "completion")
+  :maintainers '(("Daniel Mendler" . "mail@daniel-mendler.de")))
